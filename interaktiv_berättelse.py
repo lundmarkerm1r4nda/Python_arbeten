e75b5fb544dd6842d51a49fdeb9ded5aafd4e0ed
lundmarkerm1r4nda <miranda.lundmarker@elev.ga.ntig.se>
2024-10-07 14:27:07 +0200

    U

---
+++ interaktiv_berättelse.py
@@ -0,0 +1,60 @@
+#Använd input() och print()
+#• Använd variabler
+#• Använd if-else-satser för att undersöka vilket val spelaren gjort
+#• Skriv kommentarer i koden om vad de olika koderna gör.
+#• Minst en klasskamrat ska ha testat din berättelse & ha tittat på koden.
+
+ascii_title = """
+  _______ _            ____                 _     __          _                     _    _                       
+ |__   __| |          |  _ \               | |   / _|        | |                   | |  | |                      
+    | |  | |__   ___  | |_) |_ __ ___  __ _| | _| |_ __ _ ___| |_   _ __ ___  _   _| | _| |__   __ _ _ __   __ _ 
+    | |  | '_ \ / _ \ |  _ <| '__/ _ \/ _` | |/ /  _/ _` / __| __| | '_ ` _ \| | | | |/ / '_ \ / _` | '_ \ / _` |
+    | |  | | | |  __/ | |_) | | |  __/ (_| |   <| || (_| \__ \ |_  | | | | | | |_| |   <| |_) | (_| | | | | (_| |
+    |_|  |_| |_|\___| |____/|_|  \___|\__,_|_|\_\_| \__,_|___/\__| |_| |_| |_|\__,_|_|\_\_.__/ \__,_|_| |_|\__, |
+                                                                                                            __/ |
+                                                                                                           |___/ """
+
+print (ascii_title)
+
+print ("\nYou wake up hastily from what must've been a nightmare, sweaty and stressed you look around your room and try to calm yourself")
+start_the_day = input ("\nAfter what feels like an eternity of trying to fall asleep, should you stop trying and start the day? Yes/No\n").lower()
+if start_the_day == "yes":
+    print ("\nYou go down to the kitchen...")
+else:
+    print ("\nYou try for 30 more minutes. But sleep does not come your way and you decide to go down to the kitchen anyways.")
+  
+breakfast = input("\nIn the kitchen another choice awaits you. Bread or Yoghurt for breakfast?\n").lower()
+if breakfast == "bread":
+    print ("\nYou walk to the cupboard to pick bread")
+    bread_type = input("Sourdough or Wholewheat?\n").lower()
+    if bread_type == "sourdough":
+        print ("\nYou take out the Sourdough and cut 2 slices.")
+    else:
+        print ("\nYou take out the Wholewheat and cut 2 slices.")
+
+    print(f"\nYou spread butter on your {bread_type} slices")
+
+    bread_toppings = input("\nYou open the fridge to see what toppings options you have. Cheese or Ham?\n").lower()
+    if bread_toppings == "cheese":
+        print("\nYou take out the Cheese and also the slicer next to it. You cut 2 thin slices for each bread slice.")
+    else:
+        print("\nYou open the container and take out one piece of Ham for each slice of bread.")
+
+    print(f"\nYou take your {bread_type} slices with {bread_toppings} and go sit down at the table.")
+
+else:
+    yoghurt_flavor = input("\nYou walk to the fridge. What flavor Yoghurt do you want? Mango or Banana\n").lower()
+    if yoghurt_flavor == "mango":
+        print ("\nYou take out the Mango Flavored Yoghurt and pours it down into your bowl")
+    else:
+        print ("\nYou take out the Banana Flavored Yoghurt and pours it into your bowl.")
+    print (f"After pouring the {yoghurt_flavor} yoghurt into your bowl you are left to decide what cereal to have.")
+    cereals = input("Musli or Cornflakes?\n").lower()
+
+    if cereals == "musli":
+        print(f"\nYou shake the Musli down on top of your {yoghurt_flavor} yoghurt.")
+    else:
+        print(f"\nYou walk to the cupboard to take out the Cornflakes, you open the door but get jumped by a rat, turns out the rat has been sitting in here snacking on your cornflakes. But you take the cornflakes that are left and pours them on your {yoghurt_flavor} yoghurt.")
+
+    print(f"You take your {yoghurt_flavor} {breakfast} with {cereals} and go sit down at the table.")
+
--- slagsmålsspel copy.py
+++ slagsmålsspel copy.py
@@ -4,31 +4,31 @@ import random
 import time
 
 #problem - the money is not being saved, will always have 100 money in at the start.
+money = 100
+randomnames = ('Bengt' , 'Peter' , 'Agatha')
 
 while True: 
 
-    randomnames = ('Bengt' , 'Peter' , 'Agatha')
-
+    
     print ("\n================= Slagsmålsspelet =================\n")
-    Player1 = input ("Name for player 1: ")
-    Player2 = random.choice(randomnames)
-    Player1Hp = 20
-    Player2Hp = 20
-    Money = 100
+    player1 = input ("Name for player 1: ")
+    player2 = random.choice(randomnames)
+    player1Hp = 20
+    player2_hp = 20
 
     print ("Finding your opponent...")
     time.sleep(3)
-    print ("Opponent found, Your opponent is" , Player2)
+    print ("Opponent found, Your opponent is" , player2)
         
     time.sleep(3)
 
-    print ("\nYou currently have" , Money , "money.\n") 
+    print ("\nYou currently have" , money , "money.\n") 
     betting = input ("Do you want to place a bet? Yes/No\n").lower()
     if betting == "yes":
             while True:
                 try: #för att man inte ska kunna skriva in annat än nummer
                     bettamount = float(input("How much do you money do you want to bet?\n"))
-                    if bettamount > Money:
+                    if bettamount > money:
                         print ("Sorry you do not have enough money to bet that much.\n")
                     elif bettamount <= 0:
                         print ("The bet must be greater than 0, please try again.\n")
@@ -42,7 +42,7 @@ while True:
         print ("\nNo bet placed.\n")
         time.sleep(2)
 
-    while (Player1Hp >= 0 and Player2Hp >= 0):
+    while (player1Hp >= 0 and player2_hp >= 0):
         
         DamagePlayer1Basic = random.randint(1,7)
         DamagePlayer1Advanced = random.randint(10,20)
@@ -60,77 +60,77 @@ while True:
         time.sleep(1)
         if attack == "basic":
             DamagePlayer1Basic
-            print ("\n" , Player1, "made a" , attack , "attack.")
-            print (Player2, "lost" , DamagePlayer1Basic , "HP\n")
-            Player2Hp -= DamagePlayer1Basic
+            print ("\n" , player1, "made a" , attack , "attack.")
+            print (player2, "lost" , DamagePlayer1Basic , "HP\n")
+            player2_hp -= DamagePlayer1Basic
 
         if attack == "advanced":
             if random.random() < AdvancedChance:
-                print ("\n" , Player1, "made an", attack , "attack")
+                print ("\n" , player1, "made an", attack , "attack")
                 time.sleep(3)
-                print (Player2, "lost " , DamagePlayer1Advanced , "HP\n")
-                Player2Hp -= DamagePlayer1Advanced
+                print (player2, "lost " , DamagePlayer1Advanced , "HP\n")
+                player2_hp -= DamagePlayer1Advanced
             else:
-                print("\n" , Player1 , "made and Advanced attack, it missed!" , Player2 , "took 0 damage")
+                print("\n" , player1 , "made and Advanced attack, it missed!" , player2 , "took 0 damage")
 
         if attack == "healing":
-            if Player1Hp >= 20:
+            if player1Hp >= 20:
                 print ("\nHealing failed, HP is already +20\n")
             else:
                 HealingPlayer1
-                print ("\n" , Player1 , "healed up!\n")
-                Player1Hp += HealingPlayer1
+                print ("\n" , player1 , "healed up!\n")
+                player1Hp += HealingPlayer1
                 print ("you gained" , HealingPlayer1 , "HP")
-                print (Player1 , "currently has" , Player1Hp , "HP\n")
+                print (player1 , "currently has" , player1Hp , "HP\n")
 
         time.sleep(1)
-        print (Player2 , "currently has ", Player2Hp , "HP.\n")
+        print (player2 , "currently has ", player2_hp , "HP.\n")
 
-        if Player2Hp <= 0:
-            print (Player1, "won!\n")
-            Money +=  bettamount * 2
-            print ("You won the bet! Money currently in your possesion is" , Money , "\n")
+        if player2_hp <= 0:
+            print (player1, "won!\n")
+            money +=  bettamount * 2
+            print ("You won the bet! Money currently in your possesion is" , money , "\n")
             break
 
         time.sleep(3)
 
-        print (Player2, "made a/n", Player2attack , "attack")
+        print (player2, "made a/n", Player2attack , "attack")
 
 
         if Player2attack == "Basic":
             DamagePlayer2Basic
-            print (Player1, "took " , DamagePlayer2Basic , "HP\n" )
-            Player1Hp -= DamagePlayer2Basic
+            print (player1, "took " , DamagePlayer2Basic , "HP\n" )
+            player1Hp -= DamagePlayer2Basic
             time.sleep(3)
-            print (Player1 , "currently has " , Player1Hp , "HP\n")
+            print (player1 , "currently has " , player1Hp , "HP\n")
 
         if Player2attack == "Advanced":
             if random.random() < AdvancedChance:
                 DamagePlayer2Advanced
-                print (Player1 , "took" , DamagePlayer2Advanced , "damage\n")
-                Player1Hp -= DamagePlayer2Advanced
+                print (player1 , "took" , DamagePlayer2Advanced , "damage\n")
+                player1Hp -= DamagePlayer2Advanced
                 time.sleep(3)
-                print (Player1 , "currently has" , Player1Hp , "HP left\n")
+                print (player1 , "currently has" , player1Hp , "HP left\n")
             else:
-                print (Player2 , "missed,", Player1 , "took 0 damage\n")
+                print (player2 , "missed,", player1 , "took 0 damage\n")
         
         if Player2attack == "Healing":
-            if Player2Hp >= 20:
+            if player2_hp >= 20:
                 print ("\nHealing failed, HP is already +20\n")
             
             else:
                 HealingPlayer2
-                print (f"\n{Player2} healed up! \n")
-                Player2Hp += HealingPlayer2
-                print (f"{Player2} currently has {Player2Hp} HP \n")
+                print (f"\n{player2} healed up! \n")
+                player2_hp += HealingPlayer2
+                print (f"{player2} currently has {player2_hp} HP \n")
 
 
             time.sleep(3)
 
-        if Player1Hp <= 0:
-            print (f"{Player2} won!\n")
-            Money -= bettamount
-            print (f"You lost the bet, money in your possesion is currently:{Money}\n")
+        if player1Hp <= 0:
+            print (f"{player2} won!\n")
+            money -= bettamount
+            print (f"You lost the bet, money in your possesion is currently:{money}\n")
             break
 
         
