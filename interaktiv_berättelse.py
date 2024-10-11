882999165a9acf44479a03c43f590dff55c32ea5
lundmarkerm1r4nda <miranda.lundmarker@elev.ga.ntig.se>
2024-10-11 11:19:59 +0200

    L

--- interaktiv_berättelse.py
+++ interaktiv_berättelse.py
@@ -1,7 +1,3 @@
-#Använd input() och print()
-#• Använd variabler
-#• Använd if-else-satser för att undersöka vilket val spelaren gjort
-#• Skriv kommentarer i koden om vad de olika koderna gör.
 #• Minst en klasskamrat ska ha testat din berättelse & ha tittat på koden.
 
 ascii_title = """
@@ -20,7 +16,7 @@ print ("\nYou wake up hastily from what must've been a nightmare, sweaty and str
 start_the_day = input ("\nAfter what feels like an eternity of trying to fall asleep, should you stop trying and start the day? Yes/No\n").lower()
 if start_the_day == "yes":
     print ("\nYou go down to the kitchen...")
-else:
+else: #Sleep more option
     print ("\nYou try for 30 more minutes. But sleep does not come your way and you decide to go down to the kitchen anyways.")
   
 breakfast = input("\nIn the kitchen another choice awaits you. Bread or Yoghurt for breakfast?\n").lower()
@@ -29,7 +25,7 @@ if breakfast == "bread":
     bread_type = input("Sourdough or Wholewheat?\n").lower()
     if bread_type == "sourdough":
         print ("\nYou take out the Sourdough and cut 2 slices.")
-    else:
+    else: #Wholewheat option
         print ("\nYou take out the Wholewheat and cut 2 slices.")
 
     print(f"\nYou spread butter on your {bread_type} slices")
@@ -37,7 +33,7 @@ if breakfast == "bread":
     bread_toppings = input("\nYou open the fridge to see what toppings options you have. Cheese or Ham?\n").lower()
     if bread_toppings == "cheese":
         print("\nYou take out the Cheese and also the slicer next to it. You cut 2 thin slices for each bread slice.")
-    else:
+    else: #Ham option
         print("\nYou open the container and take out one piece of Ham for each slice of bread.")
 
     print(f"\nYou take your {bread_type} slices with {bread_toppings} and go sit down at the table.")
@@ -46,14 +42,14 @@ else:
     yoghurt_flavor = input("\nYou walk to the fridge. What flavor Yoghurt do you want? Mango or Banana\n").lower()
     if yoghurt_flavor == "mango":
         print ("\nYou take out the Mango Flavored Yoghurt and pours it down into your bowl")
-    else:
+    else: #Banana option
         print ("\nYou take out the Banana Flavored Yoghurt and pours it into your bowl.")
     print (f"After pouring the {yoghurt_flavor} yoghurt into your bowl you are left to decide what cereal to have.")
     cereals = input("Musli or Cornflakes?\n").lower()
 
     if cereals == "musli":
         print(f"\nYou shake the Musli down on top of your {yoghurt_flavor} yoghurt.")
-    else:
+    else: #cornflakes option
         print(f"\nYou walk to the cupboard to take out the Cornflakes, you open the door but get jumped by a rat, turns out the rat has been sitting in here snacking on your cornflakes. But you take the cornflakes that are left and pours them on your {yoghurt_flavor} yoghurt.")
 
     print(f"You take your {yoghurt_flavor} {breakfast} with {cereals} and go sit down at the table.")
